70f859d66a7af4cdbb30a9b411c7cf16963ad645
gmarseglia <g.marseglia.it@gmail.com>
2023-12-21 14:31:51 +0100

    Working serial comunication

--- src/client/client.go
+++ src/client/client.go
@@ -1,11 +1,49 @@
 package main
 
 import (
-	"fmt"
-	"mini/utils"
+	"context"
+	"flag"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "mini/proto"
+)
+
+const (
+	timeout = 5 * time.Second
+)
+
+var (
+	addr = flag.String("addr", "localhost:55555", "the address to connect to")
 )
 
 func main() {
-	fmt.Println("I'm the client")
-	fmt.Println(utils.UtilHello())
+	// parse the flags
+	flag.Parse()
+
+	// Set up a connection to the gRPC server
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	// create the client object
+	c := pb.NewMiniClient(conn)
+
+	// create the context
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	// contact the server
+	r, err := c.Choice(ctx, &pb.ChoiceBiRequest{Option1: "uno", Option2: "due", Millis: 1000})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+
+	// print the result
+	log.Printf("Greeting: %s", r.GetOption())
 }
--- src/server/server.go
+++ src/server/server.go
@@ -1,7 +1,73 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"math/rand"
+	pb "mini/proto"
+	"net"
+	"time"
+
+	"google.golang.org/grpc"
+)
+
+// define flags
+var (
+	port = flag.Int("port", 55555, "The server port")
+	seed = flag.Int("seed", 14, "The seed for the PRNG")
+)
+
+// build the PRNG
+var (
+	Prng = rand.New(rand.NewSource(int64(*seed)))
+)
+
+// server is used to implement the MiniServer interface
+type server struct {
+	pb.UnimplementedMiniServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) Choice(ctx context.Context, in *pb.ChoiceBiRequest) (*pb.ChoiceReply, error) {
+	// log response
+	log.Printf("Received: options:(%s, %s), sleep: %d", in.GetOption1(), in.GetOption2(), in.GetMillis())
+
+	// sleep
+	time.Sleep(time.Duration(in.GetMillis()) * time.Millisecond)
+
+	// randomly choose response
+	var response string
+	if Prng.Intn(2) == 0 {
+		response = in.GetOption1()
+	} else {
+		response = in.GetOption2()
+	}
+
+	// send response
+	return &pb.ChoiceReply{Option: response}, nil
+}
 
 func main() {
-	fmt.Println("I'm the server")
+	// parse the flags for CLI
+	flag.Parse()
+
+	// listen to request to specified port
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	// create a new server
+	s := grpc.NewServer()
+
+	// register the server
+	pb.RegisterMiniServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+
+	// serve the request
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
