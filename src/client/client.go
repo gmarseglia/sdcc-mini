7ceef7636bd32d8f168a196900c6235b4d4fa0bb
gmarseglia <g.marseglia.it@gmail.com>
2024-01-02 16:14:39 +0100

    Client parallel

--- src/client/client.go
+++ src/client/client.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"sync"
 	"time"
 
 	"google.golang.org/grpc"
@@ -18,24 +19,21 @@ const (
 )
 
 var (
-	frontAddr = flag.String("frontAddr", "localhost", "The address to connect to")
-	frontPort = flag.Int("frontPort", 55555, "The port of the master service")
+	frontAddr    = flag.String("frontAddr", "localhost", "The address to connect to")
+	frontPort    = flag.Int("frontPort", 55555, "The port of the master service")
+	requestCount = flag.Int("requestCount", 1, "The number of requests to send")
+	counter      int
+	counterLock  sync.Mutex
+	wg           sync.WaitGroup
+	c            pb.FrontClient
 )
 
-func main() {
-	// parse the flags
-	flag.Parse()
-
-	// Set up a connection to the gRPC server
-	serverFullAddr := fmt.Sprintf("%s:%d", *frontAddr, *frontPort)
-	conn, err := grpc.Dial(serverFullAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
-	}
-	defer conn.Close()
-
-	// create the client object
-	c := pb.NewFrontClient(conn)
+func choice() {
+	// Internal ID
+	counterLock.Lock()
+	counter++
+	id := counter
+	counterLock.Unlock()
 
 	// create the context
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
@@ -44,6 +42,8 @@ func main() {
 	// time the call
 	startTime := time.Now()
 
+	log.Printf("[Client]: Request #%d sent.", id)
+
 	// contact the server
 	r, err := c.Choice(ctx, &pb.ChoiceBiRequest{Option1: "A", Option2: "B"})
 	if err != nil {
@@ -51,5 +51,37 @@ func main() {
 	}
 
 	// print the result
-	log.Printf("Response: (#%d, %s); Time spent: %d ms", r.GetReplyID(), r.GetOption(), time.Since(startTime).Milliseconds())
+	log.Printf("[Client]: Request #%d -> Response: (#%d , %s) in %d ms", id, r.GetReplyID(), r.GetOption(), time.Since(startTime).Milliseconds())
+
+	wg.Done()
+}
+
+func main() {
+	// parse the flags
+	flag.Parse()
+
+	// Welcome message
+	log.Printf("[Main]: Welcome. Client will send %d requests in parallel.", *requestCount)
+
+	// Set up a connection to the gRPC server
+	serverFullAddr := fmt.Sprintf("%s:%d", *frontAddr, *frontPort)
+	conn, err := grpc.Dial(serverFullAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("[Main]: Could not not connect. More:\n%v", err)
+	}
+	defer conn.Close()
+
+	// create the client object
+	c = pb.NewFrontClient(conn)
+
+	for i := 0; i < *requestCount; i++ {
+		wg.Add(1)
+		go choice()
+	}
+
+	// wait
+	log.Printf("[Main]: All requests sent. Waiting for responses...")
+	wg.Wait()
+
+	log.Printf("[Main]: All requests completed. Terminating. Goodbye.")
 }
